c8c9117163ab0f738fae30ecb84299f161f944a0
“Gustavo <gusto2897@gmail.com>
2023-09-08 18:01:58 -0700

    finished hero section and nav bar

---
+++ fashion/js/main.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function ($) {
+  $(".burger-container").on("click", function (e) {
+    e.preventDefault();
+    $(this).children(".burger").toggleClass("burger--active");
+    $("#hero #top-nav > ul").toggleClass("active");
+    $("#hero #top-nav .shop-icon-and-login-container").toggleClass("active");
+  });
+});
+
+const Nav = document.querySelector("#top-nav-container");
+const NavTop = Nav.offsetTop + 100;
+
+function fixnavbar() {
+  if (window.scrollY >= NavTop) {
+    document.body.style.paddingTop = Nav.offsetHeight + "px";
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove("fixed-nav");
+  }
+}
+window.addEventListener("scroll", fixnavbar);
